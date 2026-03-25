9899c7f77def3d2eabaff3d6bdeec279133be4c5
josemanueljaimemorales <josemanueljaimemorales@gmail.com>
2026-03-25 10:08:30 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,15 @@
-function openScreen(type) {
-  document.getElementById("home").classList.remove("active");
-
-  if (type === "calendar") {
-    document.getElementById("calendarScreen").classList.add("active");
-    return;
-  }
-
-  let url = "";
-
-  if (type === "fuerza") url = "https://josemanueljaimemorales.github.io/AKC_PRO_MASTER/";
-  if (type === "rutinas") url = "https://josemanueljaimemorales.github.io/RutinasAKC/";
-  if (type === "trabajo") url = "https://josemanueljaimemorales.github.io/TRABAJOGAVAKC/";
-
-  document.getElementById("viewerFrame").src = url;
-  document.getElementById("viewerScreen").classList.add("active");
+function openScreen(type){
+if(type==="drive"){window.open("https://1drv.ms/f/c/55b6a939d4276db6/ErZtJ9Q5qbYggFX4OQAAAAABVtoRkonNs_tKnNzCeHOG7g","_blank");return;}
+document.getElementById("home").classList.remove("active");
+if(type==="calendar"){document.getElementById("calendarScreen").classList.add("active");return;}
+let url="";
+if(type==="fuerza")url="https://josemanueljaimemorales.github.io/AKC_PRO_MASTER/";
+if(type==="rutinas")url="https://josemanueljaimemorales.github.io/RutinasAKC/";
+if(type==="trabajo")url="https://josemanueljaimemorales.github.io/TRABAJOGAVAKC/";
+document.getElementById("viewerFrame").src=url;
+document.getElementById("viewerScreen").classList.add("active");
 }
-
-function goHome() {
-  document.querySelectorAll(".screen").forEach(s => s.classList.remove("active"));
-  document.getElementById("home").classList.add("active");
+function goHome(){
+document.querySelectorAll(".screen").forEach(s=>s.classList.remove("active"));
+document.getElementById("home").classList.add("active");
 }
