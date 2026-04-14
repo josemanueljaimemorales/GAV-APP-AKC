142ad824eefe7c5179b32e75b88e1ff5609dc42d
josemanueljaimemorales <josemanueljaimemorales@gmail.com>
2026-04-14 06:51:27 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 function openScreen(type){
-if(type==="cargas"){
-  window.location.href = "https://josemanueljaimemorales.github.io/Cargas-sistemas-y-fuerzas/";
-  return;
-}
-if(type==="drive"){window.open("https://1drv.ms/f/c/55b6a939d4276db6/ErZtJ9Q5qbYggFX4OQAAAAABVtoRkonNs_tKnNzCeHOG7g","_blank");return;}
-document.getElementById("home").classList.remove("active");
+if(type==="cargas")url="https://josemanueljaimemorales.github.io/Cargas-sistemas-y-fuerzas/";
 if(type==="calendar"){document.getElementById("calendarScreen").classList.add("active");return;}
 let url="";
 if(type==="fuerza")url="https://josemanueljaimemorales.github.io/AKC-CON-REPORTE/";
